bc9e9e0ce379dce2a9495da5a267bca678c91b2f
prakhar362 <prakharshri2005@gmail.com>
2025-06-25 21:56:30 +0530

    basic backend setup done

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export async function Signup(req,res){
+    res.send('Signup Endpoint !');
+}
+
+export async function Login(req,res){
+    res.send('Login Endpoint !');
+}
+
+export async function Logout(req,res){
+    res.send('Logout Endpoint !');
+}
---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import mongosse from 'mongoose';
+import dotenv from 'dotenv';
+
+//route imports
+import AuthRoutes from './routes/auth.route.js';
+
+const app=express();
+dotenv.config();
+
+//routes
+app.get('/',(req,res)=>{
+    res.send('hello from server !');
+})
+
+app.use("/api/v1/auth",AuthRoutes);
+
+//running:
+const port=process.env.PORT;
+const mongourl=process.env.MONGO_URI;
+app.listen(port, async()=>{
+    try {
+        const connection=await mongosse.connect(mongourl);
+        if(connection)
+        {
+            console.log(`Server running on http://localhost:${port}`);
+            console.log('MongoDB connected successfully')
+        }
+        
+    } catch (error) {
+        console.error('Error msg: ',error);
+    }
+    
+})
