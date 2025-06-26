4556571aa747ed9c97db1628aeebf32528af5f7a
prakhar362 <prakharshri2005@gmail.com>
2025-06-26 21:46:53 +0530

    login, logout added, need to work upstrea

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.js";
 import {z} from 'zod';
+import jwt from "jsonwebtoken";
 
 const signupSchema= z.object({
     email: z.string().email(),
@@ -40,10 +41,39 @@ export async function Signup(req,res){
     
 }
 
-export async function Login(req,res){
-    res.send('Login Endpoint !');
+export async function Login(req, res) {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(401).json({ message: "Invalid email or password" });
+
+    const isPasswordCorrect = await bcrypt.compareSync(password,user.password);
+    if (!isPasswordCorrect) return res.status(401).json({ message: "Invalid email or password" });
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true, // prevent XSS attacks,
+      sameSite: "strict", // prevent CSRF attacks
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    res.status(200).json({ success: true, user });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
 
-export async function Logout(req,res){
-    res.send('Logout Endpoint !');
+export function Logout(req, res) {
+  res.clearCookie("jwt");
+  res.status(200).json({ success: true, message: "Logout successful" });
 }
