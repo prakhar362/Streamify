b1d4e2afd5dbc6357f85e462c78c9bf9d4eefcc6
prakhar362 <prakharshri2005@gmail.com>
2025-06-27 23:13:13 +0530

    backend completed

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,24 @@ import express from 'express';
 import mongosse from 'mongoose';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from "cors";
 
 //route imports
 import AuthRoutes from './routes/auth.route.js';
+import UserRoutes from './routes/user.route.js';
+import chatRoutes from './routes/chat.route.js';
 
 const app=express();
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, // allow frontend to send cookies
+  })
+);
 
 //routes
 app.get('/',(req,res)=>{
@@ -17,6 +27,8 @@ app.get('/',(req,res)=>{
 })
 
 app.use("/api/v1/auth",AuthRoutes);
+app.use("/api/v1/user",UserRoutes);
+app.use("/api/chat", chatRoutes);
 
 //running:
 const port=process.env.PORT;
