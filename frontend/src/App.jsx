f7cb64d66d5d7245a4911133d76720827895a3bf
prakhar362 <prakharshri2005@gmail.com>
2025-06-29 10:01:49 +0530

    tailwind and daisy UI setup done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div data-theme='retro'>
+      <h1 class="text-3xl text-amber-900 font-bold underline">
+    Hello world!
+  </h1>
+  <button className="btn btn-primary">Secondary</button>
+    This div will always use light theme
+    <span data-theme="retro">This span will always use retro theme!</span>
+
+    </div>
+
+     
+     
     </>
   )
 }
