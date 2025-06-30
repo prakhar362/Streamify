17e4c3eeb70b1191588bf23d271d0e82c3299304
prakhar362 <prakharshri2005@gmail.com>
2025-06-30 21:31:16 +0530

    notifications page done

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -133,7 +133,7 @@ const HomePage = () => {
                         </span>
                       </div>
 
-                      {user.bio && <p className="text-sm opacity-70">{user.bio}</p>}
+                      {user.bio && <p className="text-md opacity-70">{user.bio}</p>}
 
                       {/* Action button */}
                       <button
