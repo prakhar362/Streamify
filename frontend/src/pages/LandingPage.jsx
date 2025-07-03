2cced9e02cfa3dcb225e2307a5f29298d6c4942c
prakhar362 <prakharshri2005@gmail.com>
2025-07-03 21:58:59 +0530

    testimonials added

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,11 +1,14 @@
 import { HeroSection } from "../components/ui/hero-section-9";
 import { FeaturesSectionWithHoverEffects } from "../components/ui/FeaturesSectionWithHoverEffects";
+import Testimonials from "../components/ui/testimonials";
+
 function LandingPage() {
   return (
     <>
     <div className="bg-white">
        <HeroSection />
      <FeaturesSectionWithHoverEffects/>
+     <Testimonials/>
     </div>
      
 
