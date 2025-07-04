6d2ec9965983bd2d0e32d8c7615e3d8391583235
prakhar362 <prakharshri2005@gmail.com>
2025-07-04 23:04:51 +0530

    added frontend in backend

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://streamify-nu-bay.vercel.app",
     credentials: true, // allow frontend to send cookies
   })
 );
